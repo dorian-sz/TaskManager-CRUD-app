76f0f486f50bfbd3a0a7a1e4827d3ecbdc147634
Dorian Szatmari <dorian.szatmari@gmail.com>
2023-04-07 22:06:38 +0200

    Add general CRUD interface

---
+++ Backend/TaskManagerApi/Service/Interface/IService.cs
@@ -0,0 +1,10 @@
+﻿namespace TaskManagerApi.Service;
+
+public interface IService<T>
+{
+    IEnumerable<T> GetAll();
+    T Get();
+    void Add(T entity);
+    void Update(T entity);
+    void Delete(long id);
+}
