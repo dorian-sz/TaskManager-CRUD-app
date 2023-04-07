ef5575c4e650478f7bc808e0f7364408ff1cb5a6
Dorian Szatmari <dorian.szatmari@gmail.com>
2023-04-07 22:08:44 +0200

    Add task entity

---
+++ Backend/TaskManagerApi/Models/Entities/UserTask.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace TaskManagerApi.Models;
+
+public class UserTask
+{
+    [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+    public long ID { get; set; }
+    public string TaskName { get; set; }
+    public string TaskDescription { get; set; }
+    public List<User> Users { get; set; } = new List<User>();
+}
