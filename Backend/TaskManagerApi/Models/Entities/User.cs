ef3b06c7b970abebed1900f489951151faaeca00
Dorian Szatmari <dorian.szatmari@gmail.com>
2023-04-07 22:07:33 +0200

    Add user entity

---
+++ Backend/TaskManagerApi/Models/Entities/User.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace TaskManagerApi.Models;
+
+public class User
+{
+    [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+    public long ID { get; set; }
+    
+    public string UserName { get; set; }
+    public string Password { get; set; }
+}
