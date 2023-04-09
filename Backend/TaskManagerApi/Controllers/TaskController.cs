0bc9d3aa6b5414bc69bf79bb1d227430f5dad628
Dorian Szatmari <dorian.szatmari@gmail.com>
2023-04-09 12:08:05 +0200

    Add Get method to task controller

--- Backend/TaskManagerApi/Controllers/TaskController.cs
+++ Backend/TaskManagerApi/Controllers/TaskController.cs
@@ -1,8 +1,30 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using TaskManagerApi.Models;
+using TaskManagerApi.Models.DTOs;
+using TaskManagerApi.Service;
 
 namespace TaskManagerApi.Controllers;
 
+[ApiController]
+[Route("/task")]
 public class TaskController : ControllerBase
 {
-    
+    private readonly ITaskService _taskService;
+
+    public TaskController(ITaskService taskService)
+    {
+        _taskService = taskService;
+    }
+
+    [HttpGet("{id}")]
+    public async Task<IActionResult> GetTask(long id)
+    {
+        var task = await _taskService.Get(id);
+        
+        if (task == null)
+        {
+            return NotFound();
+        }
+        return Ok(new { task });
+    }
 }
