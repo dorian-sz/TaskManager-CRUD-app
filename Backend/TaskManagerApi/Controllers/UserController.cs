52b43f0af180a4d6916d73232c30b01522624fe2
Dorian Szatmari <dorian.szatmari@gmail.com>
2023-04-10 20:35:40 +0200

    Add route and dependencies to UserController.cs

--- Backend/TaskManagerApi/Controllers/UserController.cs
+++ Backend/TaskManagerApi/Controllers/UserController.cs
@@ -1,8 +1,21 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using TaskManagerApi.Models;
+using TaskManagerApi.Models.DTOs;
+using TaskManagerApi.Service;
 
 namespace TaskManagerApi.Controllers;
 
+[ApiController]
+[Route("/user")]
 public class UserController : ControllerBase
 {
-    
+    private readonly IService<User> _service;
+    private readonly IUpdateService<UserDTO> _updateService;
+
+    public UserController(IService<User> service, IUpdateService<UserDTO> updateService)
+    {
+        _service = service;
+        _updateService = updateService;
+    }
+
 }
