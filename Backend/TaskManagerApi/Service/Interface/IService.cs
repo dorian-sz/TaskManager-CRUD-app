f227c9993c2b14bd53b3109dc4d8f18b5114e9d2
Dorian Szatmari <dorian.szatmari@gmail.com>
2023-04-10 09:47:36 +0200

    Add bool return value to IService for validation

--- Backend/TaskManagerApi/Service/Interface/IService.cs
+++ Backend/TaskManagerApi/Service/Interface/IService.cs
@@ -1,10 +1,12 @@
-﻿namespace TaskManagerApi.Service;
+﻿using TaskManagerApi.Models;
+
+namespace TaskManagerApi.Service;
 
 public interface IService<T>
 {
-    IEnumerable<T> GetAll();
-    T Get();
-    void Add(T entity);
-    void Update(T entity);
-    void Delete(long id);
+    Task<IEnumerable<T>> GetAll();
+    Task<T?> Get(long id);
+    Task Add(T entity);
+    Task<bool> Update(long id);
+    Task<bool> Delete(long id);
 }
