689f4ff0f0b494055b4eaf6c35490c96ed676f2a
Dorian Szatmari <dorian.szatmari@gmail.com>
2023-04-11 07:34:40 +0200

    Add user service interface

---
+++ Backend/TaskManagerApi/Service/Interface/IUserService.cs
@@ -0,0 +1,9 @@
+﻿using TaskManagerApi.Models;
+using TaskManagerApi.Models.DTOs;
+
+namespace TaskManagerApi.Service;
+
+public interface IUserService : IService<User>
+{
+    Task<User> CreateUserFromDTO(UserDTO userDto);
+}
