4d8541a9de88b688b2d17144d1c2d74cb8de7ad1
Dorian Szatmari <dorian.szatmari@gmail.com>
2023-06-09 00:35:06 +0200

    Add jsonignore to password in UserDTO.cs

--- Backend/TaskManagerApi/Models/DTOs/UserDTO.cs
+++ Backend/TaskManagerApi/Models/DTOs/UserDTO.cs
@@ -1,8 +1,11 @@
+using System.Text.Json.Serialization;
+
 namespace TaskManagerApi.Models.DTOs;
 
 public class UserDTO
 {
     public long userID { get; set; }
     public string Username { get; set; }
+    [JsonIgnore]
     public string Password { get; set; }
 }
