7afde632a968257d6a85daeffcf3cf1535d4279f
Dorian Szatmari <dorian.szatmari@gmail.com>
2023-06-09 00:32:51 +0200

    Add LoginDTO.cs

---
+++ Backend/TaskManagerApi/Models/DTOs/LoginDTO.cs
@@ -0,0 +1,9 @@
+using System.Text.Json.Serialization;
+
+namespace TaskManagerApi.Models.DTOs;
+
+public class LoginDTO
+{
+    public string Username { get; set; }
+    public string Password { get; set; }
+}
