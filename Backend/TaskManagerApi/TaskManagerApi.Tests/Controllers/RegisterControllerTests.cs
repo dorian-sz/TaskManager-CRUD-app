ccf9c2e6da4124a41b8844cac075c88970b0b6ac
Dorian Szatmari <dorian.szatmari@gmail.com>
2023-06-15 20:02:46 +0200

    Add test for Register endpoint Created result

--- Backend/TaskManagerApi/TaskManagerApi.Tests/Controllers/RegisterControllerTests.cs
+++ Backend/TaskManagerApi/TaskManagerApi.Tests/Controllers/RegisterControllerTests.cs
@@ -15,4 +15,43 @@ public class RegisterControllerTests
     {
         _userService = A.Fake<IUserService>();
     }
+    
+    
+    [Fact]
+    public async void RegisterController_Register_ReturnConflict()
+    {
+        //Arrange
+        var expectedStatusCode = 409;
+        var userDto = A.Fake<UserDTO>();
+        A.CallTo(() => _userService.UserExists(userDto.Username)).Returns(true);
+        var controller = new RegisterController(_userService);
+        
+        //Act
+        var result = await controller.Register(userDto);
+        var createdResult = result as StatusCodeResult;
+        
+        //Assert
+        A.CallTo(() => _userService.UserExists(userDto.Username)).MustHaveHappenedOnceExactly();
+        result.Should().NotBeNull();
+        Assert.Equal(expectedStatusCode, createdResult.StatusCode);
+    }
+    
+    [Fact]
+    public async void RegisterController_Register_ReturnCreated()
+    {
+        //Arrange
+        var expectedStatusCode = 201;
+        var userDto = A.Fake<UserDTO>();
+        A.CallTo(() => _userService.UserExists(userDto.Username)).Returns(false);
+        var controller = new RegisterController(_userService);
+        
+        //Act
+        var result = await controller.Register(userDto);
+        var createdResult = result as StatusCodeResult;
+        
+        //Assert
+        A.CallTo(() => _userService.UserExists(userDto.Username)).MustHaveHappenedOnceExactly();
+        result.Should().NotBeNull();
+        Assert.Equal(expectedStatusCode, createdResult.StatusCode);
+    }
 }
