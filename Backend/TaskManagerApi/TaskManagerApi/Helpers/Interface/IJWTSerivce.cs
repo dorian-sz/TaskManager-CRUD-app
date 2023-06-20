615ed2c80dda3725f274c4595140b13148bb8309
Dorian Szatmari <dorian.szatmari@gmail.com>
2023-06-20 22:15:47 +0200

    Add return tokendto when creating jwt

--- Backend/TaskManagerApi/TaskManagerApi/Helpers/Interface/IJWTSerivce.cs
+++ Backend/TaskManagerApi/TaskManagerApi/Helpers/Interface/IJWTSerivce.cs
@@ -2,10 +2,11 @@ using System.IdentityModel.Tokens.Jwt;
 using System.Security.Claims;
 using Microsoft.IdentityModel.Tokens;
 using TaskManagerApi.Models;
+using TaskManagerApi.Models.DTOs;
 
 namespace TaskManagerApi.Helper;
 
 public interface IJWTSerivce
 {
-    string Generate(User user);
+    TokenDTO Generate(User user);
 }
