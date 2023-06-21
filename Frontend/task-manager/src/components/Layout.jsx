d24b16e98c17b049f47b15c2253a98c628d9e0a1
Dorian Szatmari <dorian.szatmari@gmail.com>
2023-06-21 22:19:33 +0200

    Rename Navigation component to Navbar

--- Frontend/task-manager/src/components/Layout.jsx
+++ Frontend/task-manager/src/components/Layout.jsx
@@ -1,10 +1,10 @@
 import {Outlet} from 'react-router-dom'
-import Navigation from './Navigation'
+import Navbar from './Navbar'
 
 const Layout = () => {
   return (
     <div className='App'>
-        <Navigation/>
+        <Navbar/>
         <Outlet/>
     </div>
   )
