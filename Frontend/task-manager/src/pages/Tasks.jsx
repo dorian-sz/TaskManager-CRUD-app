bd7fc80b4d8af69655cc44c0394dffe41a6a7441
Dorian Szatmari <dorian.szatmari@gmail.com>
2023-06-22 22:50:43 +0200

    Add task component with design

---
+++ Frontend/task-manager/src/pages/Tasks.jsx
@@ -0,0 +1,39 @@
+import React, { useEffect, useState } from 'react';
+import useAuth from '../hooks/useAuth';
+import Task from '../components/Task';
+import AddTask from '../components/AddTask';
+import "./styles/Tasks.css";
+
+const taskUrl = "http://localhost:5084/api/Task/userstasks";
+
+
+const Tasks = () => {
+    const {auth} = useAuth();
+    const [tasks, setTasks] = useState([]);
+    const userID = auth?.userID;
+
+    const  getTasks = async () => {
+        let response = await fetch(`${taskUrl}/${userID}`, {
+            method : 'GET',
+            headers : {'Content-Type' : 'application/json'},
+            credentials : 'include'
+        });
+        const fetchedTasks = await response.json();
+        setTasks(fetchedTasks);
+    }
+
+    useEffect(()=>{
+        getTasks();
+        return;
+    },[])
+
+    return (
+        <div className='tasks-container'>
+            <AddTask userID={userID}/>
+            {tasks ? tasks.map((task) => <Task task={task}/>) : <></>}
+            
+        </div>
+    );
+};
+
+export default Tasks;
