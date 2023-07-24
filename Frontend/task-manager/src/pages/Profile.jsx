a38a9ea59e42cc5edff2f7a3a93c418a60cd5782
Dorian Szatmari <dorian.szatmari@gmail.com>
2023-07-24 14:02:01 +0200

    Add profile page

--- Frontend/task-manager/src/pages/Profile.jsx
+++ Frontend/task-manager/src/pages/Profile.jsx
@@ -1,8 +1,28 @@
-import React from 'react'
+import {useEffect, useState} from 'react'
+import useAuth from '../hooks/useAuth';
+import { Fetch } from '../components/Fetch';
+import ProfileCard from '../components/ProfileCard';
 
 const Profile = () => {
+  const [userData, setUserData] = useState();
+  const {auth} = useAuth();
+  const userID = auth?.userID;
+
+  const GetUserData = async () => {
+    const response = await Fetch(`User/${userID}`, "GET")
+    const fetchedUser = await response.json();
+    setUserData(fetchedUser);
+  }
+
+  useEffect(()=>{
+    GetUserData();
+    return;
+  },[])
+
   return (
-    <div>Profile</div>
+    <>
+      <ProfileCard userData={userData}></ProfileCard>
+    </>
   )
 }
 
